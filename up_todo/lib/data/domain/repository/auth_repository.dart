52b8667570a6569dfb5c76a54c75edb935b1b1fe
MansurSarkhanov <sarxanov.mansur@gmail.com>
2024-05-01 16:01:32 +0400

    local storage

--- up_todo/lib/data/domain/repository/auth_repository.dart
+++ up_todo/lib/data/domain/repository/auth_repository.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:multiple_result/multiple_result.dart';
-import 'package:up_todo/data/models/auht_error_model.dart';
+import 'package:up_todo/data/models/auth_error_model.dart';
 import 'package:up_todo/data/source/auth_data_source.dart';
 import 'package:up_todo/injection.dart';
+
+import '../../source/local_auth_data_source.dart';
 
 abstract interface class IAuthRepository {
   Future<Result<User?, AuthErrorModel>> loginUser({required String email, required String password});
@@ -10,11 +12,15 @@ abstract interface class IAuthRepository {
 }
 
 class AuthRepository implements IAuthRepository {
+  final _localAuthDataSource = getIt.get<LocalAuthDataSource>();
+
   final _authDataSource = getIt.get<AuthDataSource>();
   @override
   Future<Result<User?, AuthErrorModel>> loginUser({required String email, required String password}) async {
     try {
       final response = await _authDataSource.loginUser(email: email, password: password);
+      await _localAuthDataSource.saveToken(response!.uid);
+      
       return Success(response);
     } catch (e) {
       return Error(AuthErrorModel(message: e.toString()));
--- up_todo/lib/injection.dart
+++ up_todo/lib/injection.dart
@@ -1,8 +1,22 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:up_todo/data/source/auth_data_source.dart';
+import 'package:up_todo/data/source/local_auth_data_source.dart';
 
 final getIt = GetIt.instance;
 
 void setUp() {
   getIt.registerLazySingleton(() => AuthDataSource());
+
+
+  //Cache injection
+  getIt.registerFactory<FlutterSecureStorage>(
+    () => const FlutterSecureStorage(),
+    instanceName: 'secureStorage',
+  );
+  getIt.registerLazySingleton<ILocalAuthDataSource>(
+    () => LocalAuthDataSource(
+      secureStorage: getIt(instanceName: 'secureStorage'),
+    ),
+  );
 }
